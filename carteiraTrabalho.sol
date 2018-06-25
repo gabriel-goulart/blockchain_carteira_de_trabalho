5f1c2d9a1306b8f08decce91a132f0ebc28f7801
gabriel goulart <gabrielgoulart@MacBook-Pro-de-gabriel.local>
2018-06-24 21:05:02 -0300

    correcao na estrutura do trabalho

--- carteiraTrabalho.sol
+++ carteiraTrabalho.sol
@@ -3,8 +3,8 @@ contract CarteiraTrabalho {
     
     //atributos
     address proprietario = msg.sender; // dono do contrato
-    mapping(uint => ContratoTrabalho) contratos; // lista de contratos
-    uint numContracts; // numeros de contratos
+    mapping(uint => address) contratos; // lista de contratos
+    uint numContracts=0; // numeros de contratos
 
     /**
      * verifica se o sender é o dono da carteira de trabalho
@@ -16,22 +16,18 @@ contract CarteiraTrabalho {
     }
     
     /**
-     * Cria um contrato de trabalho
-     * se não for dono da carteira de trabalho pode criar um contrato de trabalho com o dono do contrato 
-     * */
-    function contratacao() public naoDonoDaCarteira(msg.sender) view returns(address contratoDeTrabalho){
-        contratos[numContracts] = new ContratoTrabalho(proprietario,msg.sender );
-        numContracts++;
-        return contratos[numContracts-1];
-        
-    }
-    
-    /**
      * Retorna um contrato de acordo com o identificador
      * identificadorContrato é o identificador do contrato na lista
      * */
     function getContrato(uint identificadorContrato) public view returns(address contratoDeTrabalho){
         return contratos[identificadorContrato];
+    }
+    
+    function setContrato(address identificadorContrato) public naoDonoDaCarteira(msg.sender) view returns (uint indexContrato){
+        uint valor = numContracts;
+        contratos[numContracts]= identificadorContrato;
+        numContracts++;
+        return valor;
     }
 }
 
@@ -42,11 +38,11 @@ contract ContratoTrabalho {
     address private empregado; // empregado associado ao contrato
     address private empregador; // empregador associado ao contrato
     
-    uint  salario = 2 ether; // salario do empregado
-    bytes32  funcao; // funcao do empregado
-    mapping(uint => bytes32) ferias; // lista de ferias
+    uint  salario = 2; // salario do empregado
+    string  funcao; // funcao do empregado
+    mapping(uint => string) ferias; // lista de ferias
     uint numFerias; // quantidade de ferias
-    mapping(uint => bytes32) observacao; // lista de observacoes
+    mapping(uint => string) observacao; // lista de observacoes
     uint numObservacoes; // quantidade de observacoes
     bool  contratoAtivo = true; // status do contrato de trabalho
     
@@ -55,8 +51,32 @@ contract ContratoTrabalho {
      * empregado_ é o empregado associado ao contrato
      * empregador_ é o empregador associado ao contrato
      * */
-    constructor(address empregado_ , address empregador_) public{
+     constructor(address empregado_) public {
         empregado = empregado_;
+        empregador = msg.sender;
+    } 
+    
+    /**
+     * Retorna contrato criado
+     * */
+    function getContrato()public participaContrato(msg.sender) view returns(address contrato_id){
+        return address(this);
+    }
+    
+    /**
+     * Altera o valor do empregado
+     * empregado_ é o empregado que será associado ao contrato
+     * */
+    function setEmpregado(address empregado_) public participaContrato(msg.sender){
+        empregado = empregado_;
+       
+    }
+    
+    /**
+     * Altera o valor do empregador
+     * empregador_ é o empregador que será associado ao contrato
+     * */
+    function setEmpregador(address empregador_) public participaContrato(msg.sender){
         empregador = empregador_;
     }
     
@@ -70,11 +90,20 @@ contract ContratoTrabalho {
     }
     
     /**
+     * verifica se o sender é o empregador
+     * conta é o address do sender
+     * */
+    modifier ehEmpregador(address conta)  {
+        require(conta == empregador);
+        _;
+    }
+    
+    /**
      * altera a funcao do empregado em relacao ao seu trabalho
      * funcao_ é a nova funcao do empregado
      * */
-    function mudarFuncao(bytes32 funcao_) public
-    participaContrato(msg.sender){
+    function mudarFuncao(string funcao_) public
+    ehEmpregador(msg.sender){
         funcao = funcao_;
     }
     
@@ -82,7 +111,7 @@ contract ContratoTrabalho {
      * retorna a funcao do empregado em relacao ao seu trabalho
      * */
     function getFuncao() public 
-    participaContrato(msg.sender) view returns(bytes32 funcao){
+    participaContrato(msg.sender)  view returns(string funcao_atual){
         return funcao;
     }
     
@@ -91,7 +120,7 @@ contract ContratoTrabalho {
      * salario_ é o novo salario
      * */
     function mudarSalario(uint salario_) public
-    participaContrato(msg.sender){
+    ehEmpregador(msg.sender){
         salario = salario_;
     }
     
@@ -99,7 +128,7 @@ contract ContratoTrabalho {
      * retorna o salario do empregado
      * */
     function getSalario() public
-    participaContrato(msg.sender) view returns(uint salario){
+    participaContrato(msg.sender) view returns(uint salario_atual){
         return salario;
     }
     
@@ -107,8 +136,8 @@ contract ContratoTrabalho {
      * adiciona um registro de ferias na lista de ferias
      * ferias_ é a ferias a ser adicionada
      * */
-    function registrarFerias(bytes32 ferias_) public
-    participaContrato(msg.sender) view returns(uint idFerias){
+    function registrarFerias(string ferias_) public
+    ehEmpregador(msg.sender) view returns(uint idFerias){
         numFerias++;
         ferias[numFerias] = ferias_;
         return numFerias;
@@ -119,7 +148,7 @@ contract ContratoTrabalho {
      * idferias é a ferias a ser retornada
      * */
     function getFerias(uint idferias) public
-    participaContrato(msg.sender) view returns(bytes32 ferias){
+    participaContrato(msg.sender) view returns(string ferias_){
         return ferias[idferias];
     }
     
@@ -127,8 +156,8 @@ contract ContratoTrabalho {
      * registra uma observacao
      * observacao_ a ser registrada
      * */
-    function registrarObservacao(bytes32 observacao_) public
-    participaContrato(msg.sender)  view returns(uint idobservacao){
+    function registrarObservacao(string observacao_) public
+    ehEmpregador(msg.sender)  view returns(uint idobservacao){
         numObservacoes++;
         observacao[numObservacoes] = observacao_;
         return numObservacoes;
@@ -139,7 +168,7 @@ contract ContratoTrabalho {
      * idobservacao a ser retornado
      * */
     function getObservacao(uint idobservacao) public
-    participaContrato(msg.sender) view returns(bytes32 observacao){
+    participaContrato(msg.sender) view returns(string observacao_){
        return observacao[idobservacao];
     }
     
@@ -147,7 +176,7 @@ contract ContratoTrabalho {
      * registra uma demissao 
      * */
     function registrarDemissao() public
-    participaContrato(msg.sender){
+    ehEmpregador(msg.sender){
         contratoAtivo = false;
     }
     
@@ -155,7 +184,9 @@ contract ContratoTrabalho {
      * retorna o stauts do contrato
      * */
     function getStatus() public
-    participaContrato(msg.sender) view returns(bool ContratoAtivo){
+    participaContrato(msg.sender) view returns(bool ContratoAtivo_){
        return contratoAtivo;
     }
+    
+   
 }
